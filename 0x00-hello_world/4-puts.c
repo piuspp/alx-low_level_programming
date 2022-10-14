0fcd0f9f8874dc768b305bf43288ac0e0db04ecd
piuspp <go4pius@yahoo.com.com>
2022-10-14 02:51:21 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  *main - entry point
  *
  *Retur: Always 0(success)
  */
-
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");  
-  return (0);
+puts("\"Programming is like buildinga multilingual puzzle");  
+return (0);
 }
