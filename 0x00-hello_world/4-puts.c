cdaaa9200aeab9586e219df1875b0cfb48925063
piuspp <go4pius@yahoo.com.com>
2022-10-13 21:54:53 -0700

    Hello, Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Retur: Always 0(success)
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+}
