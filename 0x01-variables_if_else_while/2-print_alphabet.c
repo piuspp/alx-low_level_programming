ed9bb4ed86b47201cd30477b1f3f153a6393d0d5
piuspp <go4pius@yahoo.com.com>
2022-10-15 14:51:00 -0700

    Alphabeths

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry point
+* Description: prints the alphabet in lowercase\n
+* Return: Always 0 (success)
+*/
+int main(void)
+
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+		putchar('\n');
+	return (0);
+}
