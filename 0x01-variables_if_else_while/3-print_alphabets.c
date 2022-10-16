37f60bc578b87541a99e314ce7538037b7978e4c
piuspp <go4pius@yahoo.com.com>
2022-10-16 01:08:41 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,21 +5,18 @@
  * Description: prints the alphabet in lowercase
  * Return: Always 0 (Success)
  */
-
-int main(void);
+int main(void)
 
 {
-        int ch;
+	int x;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-
+	for (x = 'a'; x <= 'z'; x++)
 	{
-		putchar(ch);
+		putchar(x);
 	}
-
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	}
-		putchar(ch);
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
 	}
 	putchar('\n');
 	return (0);
