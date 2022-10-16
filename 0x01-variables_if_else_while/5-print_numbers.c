ba9dfc590c6b3ea2d35ad4d6d018f7b8e9d458db
piuspp <go4pius@yahoo.com.com>
2022-10-16 02:57:13 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,12 +8,12 @@
 int main(void)
 
 {
-	int x = 0;
+	int x;
 
-	while (x < 10)
+	for (x = 0; x < 10; x++)
 	{
-		printf("%d \n", x);
-		x++;
+		printf("%d", x);
 	}
+	putchar('\n');
 	return (0);
 }
