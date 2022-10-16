9ab5859f9856b242dee2f32962c29d786c56928c
piuspp <go4pius@yahoo.com.com>
2022-10-16 02:14:06 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int x = 0;
+
+	while (x > 10)
+	{
+		printf("%d \n", x);
+	}
+x++;
+	return (0);
+}
