96dc3379877d0f10b3f7ea5bbbfdfe04575c7b1d
piuspp <go4pius@yahoo.com.com>
2022-10-16 19:08:35 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * *Description: prints char below 10
+ * Return:Always 0 (success)
+ */
+int main(void)
+
+{
+	int a;
+
+	for (x = 0; x < 10; x++)
+
+}
