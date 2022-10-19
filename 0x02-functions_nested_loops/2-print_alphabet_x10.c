01894b21dc1f29d45f48137c3dc13e3e25faa7f0
piuspp <go4pius@yahoo.com.com>
2022-10-19 16:28:56 -0700

    alpha x 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - funton to print alph 10mplaces
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+	char x, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
