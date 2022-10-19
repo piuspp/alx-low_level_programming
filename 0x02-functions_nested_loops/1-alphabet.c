652574033c917b6a24f1d4c49e3ec39ac43672e8
piuspp <go4pius@yahoo.com.com>
2022-10-19 15:30:14 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabets
+ * would retuen 0 if executed correctly 
+ */
+
+void print_alphabet(void);
+{
+	char x;
+
+	for (x = 'a'; x <= 'z; x++)
+	{
+		_putchar(x);
+
+	}
+	     	
+	_putchar('\n')
+}
