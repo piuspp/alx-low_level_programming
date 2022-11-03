50e46f1a1de332d18bc6cf15dbdf6926800a0ad9
piuspp <go4pius@yahoo.com.com>
2022-11-03 03:12:17 -0700

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include  "main.h"
+/*
+ * main - a function that prints a string
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (strlen (char *s) == 0)
+		return (0);
+	puts("%c \n", char *s)
+}
