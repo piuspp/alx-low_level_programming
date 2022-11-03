2a8dc29ca0d7d79f3ec57673591814621ac9e6ab
piuspp <go4pius@yahoo.com.com>
2022-11-03 10:33:51 -0700

    Recursion

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  *
  * main - check the code
@@ -12,8 +10,9 @@
 
 int main(void)
 
-{  _print_rev_recursion("\nColton Walker");
-
-	        return (0);
+{
+	_print_rev_recursion("\nColton Walker");
+	
+	return (0);
 
 }
