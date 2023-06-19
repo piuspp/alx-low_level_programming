541e8d9f8eeec8969b035d3005bc41bb907af7ab
root <go4pius@yahoo.com>
2023-06-19 04:36:47 -0700

    Combination of three digit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        int i, j, k;
-         
-        for (i = 0; i < 10; i++)
-        {
-        for (j = 1; j < 10; j++)
-        {
-        for (k = 1; k < 10; k++)
-        {
-                if ((i > j) | (i == j) | (i > k ))
-                {
-                    continue;
-                }
-                 if ((j > k) | (j == k)) 
-                {
-                    continue;
-                }
-        
-        putchar(i + '0');
-        putchar(j + '0');
-        putchar(k + '0');
-                if ((i != 7) | (j != 8) | (k != 9))
-                {
-                    putchar(',');
-                    putchar(' '); 
-                }
-        }
-        }
-        }
-        putchar('\n');
-        return (0);
+	int i, j, k;
+
+	for (i = 0; i < 9; i++)
+	{
+	for (j = 1; j < 10; j++)
+	{
+	for (k = 1; k < 10; k++)
+	{
+		if ((i > j) | (i == j) | (i > k))
+		{
+			continue;
+		}
+		if ((j > k) | (j == k))
+		{
+			continue;
+		}
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(k + '0');
+		if ((i != 7) | (j != 8) | (k != 9))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
