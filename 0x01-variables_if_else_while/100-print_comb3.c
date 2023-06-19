9b355ab194aee684840bfbf8e38266e3eec5668d
root <go4pius@yahoo.com>
2023-06-19 02:27:20 -0700

    Combination of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int i, j;
+
 	for (i = 0; i < 9; i++)
 	{
 	for (j = 1; j < 10; j++)
@@ -16,7 +18,6 @@ int main(void)
 		{
 			continue;
 		}
-	
 	putchar(i + '0');
 	putchar(j + '0');
 		if ((i != 8) | (j != 9))
