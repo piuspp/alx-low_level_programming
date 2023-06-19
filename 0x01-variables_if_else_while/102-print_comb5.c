3f6023a946c5089b8c92189f907ccbf32a388860
root <go4pius@yahoo.com>
2023-06-19 11:54:15 -0700

    Two two digit number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-    int i, j, k, x;
-     
-    for (i = 0; i < 10; i++)
-    {
-    for (j = 0; j < 10; j++)
-    {
-    for (k = 0; k < 10; k++)
-    {
-    for (x = 0; x < 10; x++)
-    {
-       
-        if (i > j | i == j | i > k | i == k | i > x )
-        {
-            continue;
-        }
-        if (j > k | j == k | j > x) 
-        {
-            continue;
-        }
-        if (k > x ) 
-        {
-            continue;
-        }
-    
-    putchar(i + '0');
-    putchar(j + '0');
-    putchar(k + '0');
-    putchar(x + '0');
-            if (i != 7 | j != 8 | k != 9)
-            {
-                putchar(',');
-                putchar(' '); 
-    }       }
-    }
-    }
-    }
-    putchar('\n');
-    return (0);
+	int i, j, k;
+
+	for (i = 0; i < 9; i++)
+	{
+	for (j = 1; j < 10; j++)
+	{
+	for (k = 1; k < 10; k++)
+	{
+		if ((i > j) | (i == j) | (i > k))
+		{
+			continue;
+		}
+		if ((j > k) | (j == k))
+		{
+			continue;
+		}
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(k + '0');
+		if ((i != 7) | (j != 8) | (k != 9))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
