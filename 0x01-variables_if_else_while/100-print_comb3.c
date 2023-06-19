8396f135ebd5c700199a4fdeb4a621de9a761096
root <go4pius@yahoo.com>
2023-06-19 01:38:04 -0700

    Combination of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,19 @@
 int main(void)
 {
 	int i, j;
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
 	for (j = 1; j < 10; j++)
 	{
-		if (i > j && i + j == j + i)
+		if (i > j)
 		{
 			continue;
 		}
-	}
+	
 	putchar(i + '0');
 	putchar(j + '0');
 	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
