7193d5cf292abbe0d15437d2af7e9dd5ab5dc406
root <go4pius@yahoo.com>
2023-06-21 14:29:26 -0700

    Last Digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,8 +13,8 @@ int print_last_digit(int num)
 	last_digit = num % 10;
 	if (num < 0)
 	{
-		last_digit = last_digit * -1
+		last_digit = last_digit * -1;
 	}
-	_putchar(last + '0');
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
