9b2e09400ce2ffcfd66cc9bfbe1aa30ab1ebc233
root <go4pius@yahoo.com>
2023-06-21 13:59:53 -0700

    Last Digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,13 @@
 
 /**
  * print_last_digit - print the last digit of a number
- * @i: integer variable
+ * @num: integer variable
  * Return: Always 0 (Success)
  */
 int print_last_digit(int num)
 {
 	int last_digit;
 
-	last_digit = num / 10;
+	last_digit = num % 10;
 	return (last_digit);
 }
