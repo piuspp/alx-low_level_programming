9092e1d31e0e72f439360dd7ddd0061fcfc012f6
root <go4pius@yahoo.com>
2023-06-21 13:42:58 -0700

    Absolute Value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-i * _abs - function that compute absolute value of an integer
+ * _abs - function that compute absolute value of an integer
  *  @c: is the int that will use for argument
  *  Return: Always 0 (Success)
  */
@@ -10,7 +10,8 @@ int _abs(int c)
 
 {
 	int abs_val;
+
 	abs_val = abs(c);
-	return abs_val;
+	return (abs_val);
 
 }
