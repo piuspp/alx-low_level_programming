976fcb15ecfd9a8006d55fb5cacd7d1c48da8b71
root <go4pius@yahoo.com>
2023-06-22 22:33:10 -0700

    Lagest of two file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,30 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first int
- * @b: second int
- * @c: third int
- * Return: largest number
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 
-int largest_number(int a, int b, int c)
+int main(void)
 {
+	int i;
 
-int largest;
+	printf("Infinite loop incoming :(\n");
 
-if (a >= b && a >= c)
-{
-largest = a;
-}
-else if (b >= a && b >= c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+	i = 0;
+	/**
+	*while (i < 10)
+	*{
+	*       putchar(i);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
 
-return (largest);
+	return (0);
 }
