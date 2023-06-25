f6e9e8236bde3c64915b3e4940fc98bae7118069
root <go4pius@yahoo.com>
2023-06-25 00:49:11 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 24_hours - prints every minute of the day
+ * @i,j,k,x: integer variable
+ * Return: always 0 (success)
+ */
+void jack_bauer(void)
+{
+
+	int i, j, k, x;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 4; j++)
+		{
+			if (j == 4)
+			{
+				continue;
+			}
+			for (k = 0; k < 6; k++)
+			{
+				for (x = 0; x < 10; x++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar (':');
+					_putchar(k + '0');
+					_putchar(x + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
