c6ccd6bc014340cf9c50d22b21afdffb847ccde8
root <go4pius@yahoo.com>
2023-06-25 13:23:43 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two integers
+ * @i: integer variable
+ * @j: integer variable
+ * Return: always 0n(success)
+ */
+
+int add(int i, int j)
+{
+	int  k;
+
+	k = (i + j);
+	_putchar (k + '0');
+
+	return (k);
+}
+
