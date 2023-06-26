6f6231f95109964b278a2cf6e24e7af677265b6b
root <go4pius@yahoo.com>
2023-06-25 19:57:04 -0700

    Print Num

---
+++ 0x02-functions_nested_loops/11-print_to_982.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - To print natural numbers n to 98
+ * @n: An integer variable
+ * return: always 0 (success)
+ */
+
+void print_to_98(int n)
+{       
+        int i;
+
+        for (i = n: i <= 98; i++)
+        {       
+                _putchar(i + '0');
+                _putchar(',');
+                _putchar(' ');
+        }
+        return (i);
+}
