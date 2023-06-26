68772cf2e5991478bfdc17b8b9f3763f1d540c09
root <go4pius@yahoo.com>
2023-06-25 21:01:57 -0700

    Nature

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints multiples of 3 and 5
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j = j + i;
+			putchar(j + '0');
+		}
+	}
+
+	return (0);
+}
+
