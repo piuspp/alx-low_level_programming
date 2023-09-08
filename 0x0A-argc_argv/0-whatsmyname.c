304ee127daccae9c4410b3e0866ebad0ee0915f2
piuspp <pius.mbaebie@gmail.com>
2023-09-08 08:14:23 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print program name
+ * main - prints program name
  * @argc: argument count
  * @argv: argument vector
  * Return: always  0
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
+
 	printf("%s\n", argv[0]);
-		return (0);
+	return (0);
 }
