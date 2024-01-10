ed9aedf1b28913fa240c67003ed2c502fe00965c
piuspp <go4pius@yahoo.com>
2024-01-10 14:36:13 -0800

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints the output string
+ * @s: output string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
