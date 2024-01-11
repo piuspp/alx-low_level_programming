f318d7c807fc917ae1e51ed685b45d95509a4535
piuspp <go4pius@yahoo.com>
2024-01-11 01:32:18 -0800

    Function Pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with constant byte
- * @s: address to memory of array
- * @b: constant character byte
- * @n: no of bytes in array to be filled
- * Return: returns pointer with new byte
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	char *new = s;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; n > 0; i++, n--)
 	{
-		new[i] = b;
+		s[i] = b;
 	}
-	return (new);
+
+	return (s);
 }
