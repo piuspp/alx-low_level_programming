ceec5526a1526358befa203fd4efb4c0f382aee6
piuspp <go4pius@yahoo.com>
2024-02-05 18:57:48 -0800

    More Singly Linked List

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,47 +1,48 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: pointer to the beginning of linked list
- * @idx: index where to insert
- * @n: value for n
- * Return: address of new node or NULL if fails
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: head of linked list
+ * @idx: index to insert the node
+ * @n: new node data
+ * Return: new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *hold = *head;
-	unsigned int i = 0;
+	listint_t *current;
+	listint_t *new;
 
-	if (!idx)
+	if (head == NULL)
+		return (0);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+
+	new->next = NULL;
+	new->n = n;
+
+	if (idx == 0)
 	{
-		new = malloc(sizeof(listint_t));
-		if (!new)
-			return (NULL);
-
-		(*new).n = n;
-		(*new).next = *head;
-
-		*head = new;
-
-		return (*head);
+		new->next = *head;
+		(*head) = new;
+		return (new);
 	}
 
-	while (hold)
+	current = *head;
+
+	while (idx != 1)
 	{
-		if (i == (idx - 1))
+		current = current->next;
+		--idx;
+		if (current == NULL)
 		{
-			new = malloc(sizeof(listint_t));
-			if (!new)
-				return (NULL);
-			(*new).n = n;
-			(*new).next = (*hold).next;
-			(*hold).next = new;
-			return (new);
+			free(new);
+			return (NULL);
 		}
-		hold = (*hold).next;
-		i++;
 	}
-	return (NULL);
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
 }
