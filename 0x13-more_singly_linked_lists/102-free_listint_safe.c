bc7a89109c57df4e48494066369d228aa0a7bbba
piuspp <go4pius@yahoo.com>
2024-02-05 19:09:11 -0800

    More Singly Linked List

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,65 +1,47 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * _ra - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list
- *
- * Return: pointer to the new list
- */
-listint_t **_ra(listint_t **list, size_t size, listint_t *new)
-{
-	listint_t **newlist;
-	size_t i;
-
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
-	{
-		free(list);
-		exit(98);
-	}
-	for (i = 0; i < size - 1; i++)
-		newlist[i] = list[i];
-	newlist[i] = new;
-	free(list);
-	return (newlist);
-}
-
-/**
- * free_listint_safe - frees a listint_t linked list.
- * @head: double pointer to the start of the list
- *
+ * print_listint_safe - function that prints a listint_t linked list
+ * @head: pointer to the beginning of linked list
  * Return: the number of nodes in the list
  */
-size_t free_listint_safe(listint_t **head)
-{
-	size_t i, num = 0;
-	listint_t **list = NULL;
-	listint_t *next;
 
-	if (head == NULL || *head == NULL)
-		return (num);
-	while (*head != NULL)
+size_t print_listint_safe(const listint_t *head)
+{
+	int i, flag = 0;
+	listint_t *slow, *fast;
+
+	if (!head)
+		exit(98);
+	for (i = 1; (*head).next && !flag; head = (*head).next, i++)
 	{
-		for (i = 0; i < num; i++)
+		if ((*head).next)
+			slow = (*head).next;
+		if ((*head).next->next)
+			fast = (*head).next->next;
+		while (slow != fast)
 		{
-			if (*head == list[i])
-			{
-				*head = NULL;
-				free(list);
-				return (num);
-			}
+			if (slow)
+				slow = (*slow).next;
+			if (fast == head)
+				flag = 1;
+			if (fast && !flag)
+				fast = (*fast).next;
+			if (fast == head)
+				flag = 1;
+			if (fast && !flag)
+				fast = (*fast).next;
+			if (fast == head)
+				flag = 1;
 		}
-		num++;
-		list = _ra(list, num, *head);
-		next = (*head)->next;
-		free(*head);
-		*head = next;
+		printf("[%p] %d\n", (void *)head, (*head).n);
 	}
-	free(list);
-	return (num);
+
+	for (; flag && (*head).next != fast; i++, head = (*head).next)
+		printf("[%p] %d\n", (void *)head, (*head).n);
+	printf("[%p] %d\n", (void *)head, (*head).n);
+	if (fast)
+		printf("-> [%p] %d\n", (void *)fast, (*fast).n);
+	return (i);
 }
